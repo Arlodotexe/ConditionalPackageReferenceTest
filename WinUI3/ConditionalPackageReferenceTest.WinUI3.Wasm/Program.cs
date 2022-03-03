48fb931cede000915b473d19047893e0a0c1ca6d
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-03-03 17:37:59 -0600

    Initial commit

---
+++ WinUI3/ConditionalPackageReferenceTest.WinUI3.Wasm/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+using Microsoft.UI.Xaml;
+
+namespace ConditionalPackageReferenceTest.Wasm
+{
+	public class Program
+	{
+		private static App _app;
+
+		static int Main(string[] args)
+		{
+			Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
+
+			return 0;
+		}
+	}
+}
